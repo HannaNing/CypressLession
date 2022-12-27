38a3d3e189e501d0b9784fe8635f76efe36cdc05
HannaNing <ninhhoa100@gmail.com>
2022-12-27 17:23:03 +0700

    Update Submit form lession

--- cypress/e2e/SubmitForm/SubmitForm.cy.js
+++ cypress/e2e/SubmitForm/SubmitForm.cy.js
@@ -1,29 +1,36 @@
+import SubmitClass from "../../support/SubmitClass";
+
 const xpath = require('cypress-xpath')
+
+const submitClass = new SubmitClass()
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
     return false
 })
 
-describe("TODO App > ", () => {
+describe("TODO App > ", (defaultCommandTimeout=1000) => {
     it("Check First Child", () => {
+        cy.viewport(1920,1080)
         cy.visit("https://demoqa.com/automation-practice-form")
-        cy.wait(5000)
 
-        cy.xpath("//input[@id='firstName']").type('Hanna')
-        cy.xpath("//input[@id='lastName']").type('Ning')
-        cy.xpath("//input[@id='userEmail']").type('ninhhoa@gmail.com')
-        cy.xpath("//input[@id='gender-radio-2']").should('be.checked')
-        cy.xpath("//input[@id='userNumber']").type('0987654321')
-        cy.xpath("//input[@id='dateOfBirthInput']").type('01 May 1994')
-        cy.xpath("//div[@class='subjects-auto-complete__control css-yk16xz-control']").type('Math')
-        cy.xpath("//label[@for='hobbies-checkbox-3']").should('be.checked')
-        cy.xpath("//textarea[@id='currentAddress']").type('So 9, Duy Tan, Ha Noi')
-        cy.xpath("//div[@class='css-1wa3eu0-placeholder']").click()
-        cy.xpath("//div[@class='css-1uccc91-singleValue']").click()
+        submitClass.getInputFirstName().type('Hanna')
+        submitClass.getInputLastName().type('Ning')
+        submitClass.getInputEmail().type('ninhhoa@gmail.com')
+        submitClass.getInputGender().click({force:true})
+        submitClass.getInputPhoneNumber().type('0987654321')
+        submitClass.getInputDateOfBirth().click()
+        submitClass.getSelectYearOfBirth().select('1994',{force:true})
+        submitClass.getSelectMonthOfBirth().select('May',{force:true})
+        submitClass.getSelectDayOfBirth().click()
+        submitClass.getInputSubject().type('Math{enter}')
+        submitClass.getInputHobbi().check({force:true})
+        submitClass.getInputAddess().type('So 9, Duy Tan, Ha Noi')
+        submitClass.selectState().type('NCR{enter}')
+        submitClass.selectCity().type('Delhi{enter}')
+        submitClass.clickSubmit().click()
 
-        cy.xpath("//div[@class='css-yk16xz-control']").click()
-        cy.xpath("//div[@class=' css-1uccc91-singleValue']").click()
 
     })
 });
---
+++ cypress/support/SubmitClass.js
@@ -0,0 +1,49 @@
+class SubmitClass{
+    getInputFirstName(){
+        return cy.xpath("//input[@id='firstName']")
+    }
+    getInputLastName(){
+        return cy.xpath("//input[@id='lastName']")
+    }
+    getInputEmail(){
+        return cy.xpath("//input[@id='userEmail']")
+    }
+    getInputGender(){
+        return cy.xpath(`//input[@id='gender-radio-2' and @value = 'Female']`)
+    }
+    getInputPhoneNumber(){
+        return cy.xpath("//input[@id='userNumber']")
+    }
+    getInputDateOfBirth(){
+        return cy.xpath("//input[@id='dateOfBirthInput']")
+    }
+    getSelectYearOfBirth(){
+        return cy.xpath("//select[@class='react-datepicker__year-select']")
+    }
+    getSelectMonthOfBirth(){
+        return cy.xpath("//select[@class='react-datepicker__month-select']")
+    }
+    getSelectDayOfBirth(){
+        return cy.xpath("//div[@class='react-datepicker__day react-datepicker__day--001 react-datepicker__day--weekend']")
+    }
+    getInputSubject(){
+        return cy.xpath("//div[@class='subjects-auto-complete__control css-yk16xz-control']")
+    }
+    getInputHobbi(){
+        return cy.xpath("//input[@id='hobbies-checkbox-3']")
+    }
+    getInputAddess(){
+        return cy.xpath("//textarea[@id='currentAddress']")
+    }
+    selectState(){
+        return cy.xpath("//div[@id='state']")
+    }
+    selectCity(){
+        return cy.xpath("//div[@id='city']")
+    }
+    clickSubmit(){
+        return cy.xpath("//button[@id='submit']")
+    }
+
+}
+export default SubmitClass
